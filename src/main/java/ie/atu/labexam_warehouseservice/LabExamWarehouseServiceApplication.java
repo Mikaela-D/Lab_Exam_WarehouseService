58a6d63f22b438d4515287e8d732c19a20a92560
Mikaela <G00401392@atu.ie>
2023-11-08 14:43:26 +0000

    Created new project

---
+++ src/main/java/ie/atu/labexam_warehouseservice/LabExamWarehouseServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam_warehouseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamWarehouseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamWarehouseServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/labexam_warehouseservice/LabExamWarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam_warehouseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamWarehouseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
